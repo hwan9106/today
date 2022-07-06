028b065637864c66f1f567fc7f93b01e3e89f9fc
hwan9532 <hwan9532@naver.com>
2022-07-06 17:18:36 +0900

    no message

---
+++ Spring/1일차/Ex01_BootHelloWorld/src/main/java/kr/human/hello/Ex01BootHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package kr.human.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex01BootHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex01BootHelloWorldApplication.class, args);
+	}
+
+}
---
+++ Spring/1일차/Ex01_BootHelloWorld/src/test/java/kr/human/hello/Ex01BootHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.human.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex01BootHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
